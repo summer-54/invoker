faf8bd244d89c7611cc23e853f3e868099d41926
Yankovskiy Dmitriy <dmityank@gmail.com>
2025-08-15 09:03:11 +0300

    added something

--- src/judge.rs
+++ src/judge.rs
@@ -3,7 +3,9 @@ use crate::Result;
 use std::sync::Arc;
 
 pub fn judge(app: Arc<App>, packege_path: &str) -> Result<()> {
-    todo!()
+    let (test_count) = todo!("parse package");
+
+    for i in 0..test_count {}
 }
 
 pub fn stop_all(app: Arc<App>) -> Result<()> {
